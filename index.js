fa7f5c491ca96cd9ca234820c7ce5c50d4e7e79e
Rimsha Ansari <159952102+leadership286@users.noreply.github.com>
2024-03-25 23:42:47 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    { name: "UserGuessedNumber",
+        type: "number",
+        message: "Kindly Guessed a number between 1 - 6 "
+    }
+]);
+if (answer.UserGuessedNumber === randomNumber) {
+    console.log("Congratulations! Dear Player You Guessed the Correct Number.Thanks for Playing.");
+}
+else {
+    console.log("Sorry! You guessed Wrong Number Thanks for playing.");
+}
+;
